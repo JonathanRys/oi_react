1d438f95649adab6a6d2db4bb516d49bdbe6b9af
Jonathan Rys <jonathan.rk.rys@gmail.com>
2017-11-29 21:51:12 -0500

    Rename all side-bars sidebar

--- src/Components/Components.js
+++ src/Components/Components.js
@@ -62,11 +62,11 @@ class SideBar extends Component {
 
   render() {
     return (
-      <aside className={`side-bar ${this.state.isOpen ? "sidebar-open" : "sidebar-closed" }`}>
+      <aside className={`sidebar ${this.state.isOpen ? "sidebar-open" : "sidebar-closed" }`}>
         <div onClick={this.toggle} className="tab" title={!this.state.isOpen ? this.props.title : null} >
           <FontAwesome name={this.state.isOpen ? "chevron-right" : "chevron-left"} />
         </div>
-        <header className="side-bar-header">{this.props.title}</header>
+        <header className="sidebar-header">{this.props.title}</header>
         <div className="sidebar-container">
           <div className="scroller">
             {
